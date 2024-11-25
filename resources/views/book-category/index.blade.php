6cd2ab082ff0ed5423c2c61e74ff5bbfd2bdc500
unknown <rijal.1344@gmail.com>
2024-11-25 18:49:35 +0700

    layout: add dashboard layout

---
+++ resources/views/book-category/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.dashboard')
+@section('content')
+    <div class="container-fluid px-4">
+        <p>hello world</p>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Web\WebBookCategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('auth.login');
+});
+
+Route::prefix('book-category')->group(function () {
+    Route::get('/', [WebBookCategoryController::class, 'index'])->name('book-category.index');
 });
